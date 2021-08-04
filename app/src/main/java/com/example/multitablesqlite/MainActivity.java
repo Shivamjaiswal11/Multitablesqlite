d15d3b061b00cf0416ff9ab6377411a2e7939912
shivam <shivamjaiswal1191996.com>
2021-08-04 16:17:32 +0530

    onclick

--- app/src/main/java/com/example/multitablesqlite/MainActivity.java
+++ app/src/main/java/com/example/multitablesqlite/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.multitablesqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 EditText et_price,et_product,et_qty;
@@ -28,7 +30,25 @@ DatabaseHelper databaseHelper;
         spinner=findViewById(R.id.spinner);
         databaseHelper =new DatabaseHelper(this);
         databaseHelper.getReadableDatabase();
+         bt_add.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 String product=et_product.getText().toString();
+                 String qty=et_qty.getText().toString();
+                 String price=et_price.getText().toString();
+                 if(product.equals("")||qty.equals("")||price.equals("")){
+                     Toast.makeText(MainActivity.this, "all field requred", Toast.LENGTH_SHORT).show();
+                 }
+                 else{
+                     Boolean q=databaseHelper.insert(product,qty,price);
+                     if(q==true){
+                         Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                     }else {
+                         Toast.makeText(MainActivity.this, "unsuccess", Toast.LENGTH_SHORT).show();
+                     }
 
-        String[] spinnerlist = new String[]{"Product","Qty","Price"};
+                 }
+             }
+         });
     }
 }
